bf68a5535e12bff7d685b4d4d62ae44e9ef5e5d4
Mandarakpnaik <mandaranaik.xworkz@gmail.com>
2025-08-11 17:45:19 +0530

    java program

---
+++ Animals.java
@@ -0,0 +1,16 @@
+class Animals{
+	public static void main(String [] args)
+	{
+		String AnimalName = "Lion";
+		int age=13;
+		float heigth = 2.68f;
+		boolean isAlive = true;
+		byte legs = 4;
+		char gender='M';
+		System.out.println("AnimalName is ="+AnimalName);
+		System.out.println("Lion age is ="+age);
+		System.out.println("Lion is Alive = "+isAlive);
+		System.out.println("number of legs ="+legs);
+		System.out.println("gender is ="+gender);
+	}
+}
