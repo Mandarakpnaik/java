abe1b49ef87df8686a929fa843639e6e5e2b189e
Mandarakpnaik <mandaranaik.xworkz@gmail.com>
2025-08-25 18:08:02 +0530

    static class

---
+++ static class/ScienceRunner.java
@@ -0,0 +1,38 @@
+class ScienceRunner
+{
+public static void main(String [] args)
+{
+	System.out.println(Science.subjectTeacherId);
+	System.out.println("---------------");
+    
+	System.out.println(Science.subjectName);
+	System.out.println("---------------");
+
+	System.out.println(Science.teacherPhoneNumber);
+	System.out.println("---------------");
+
+	System.out.println(Science.teacherSalary );
+	System.out.println("---------------");
+
+	System.out.println(Science.teacherHeigth);
+	System.out.println("---------------");
+
+	System.out.println(Science.section);
+	System.out.println("---------------");
+
+	System.out.println(Science.experienceYears);
+	System.out.println("---------------");
+
+	System.out.println(Science.numberOfStudents);
+	System.out.println("---------------");
+
+	System.out.println(Science.isLabAvailable);
+
+
+	
+	
+	
+	
+	
+}
+}
