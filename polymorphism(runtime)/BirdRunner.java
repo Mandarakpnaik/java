d179bb3ffd9d17cec8b6baa672f84b374b7ddaea
Mandarakpnaik <mandaranaik.xworkz@gmail.com>
2025-10-05 16:37:53 +0530

    polymorphism(runtime)

---
+++ polymorphism(runtime)/BirdRunner.java
@@ -0,0 +1,15 @@
+class BirdRunner
+{
+	public static void main(String [] ref)
+	{
+		Parrot parrot = new Parrot();
+		parrot.displayColor("Green");
+		parrot.displayEggsPerYear(23455);
+		parrot. displayId(6788765l);
+		parrot.displaySymbol('P');
+		parrot.displayHeight(1.2f);
+		parrot.displayWeight(12.32);
+		parrot.displayCanFly(true);
+		
+	}
+}
