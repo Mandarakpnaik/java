6a1c8dab3f1284c2e008cfeb583ff4e9eb8708a4
Mandarakpnaik <mandaranaik.xworkz@gmail.com>
2025-10-09 13:24:48 +0530

    rbi

---
+++ rbi/src/com/xworkz/rbi/internal/CaneraBank.java
@@ -0,0 +1,8 @@
+package com.xworkz.rbi.internal;
+
+public class CaneraBank extends Rbi{
+    @Override
+    public void gst() {
+
+    }
+}
---
+++ rbi/src/com/xworkz/rbi/internal/Rbi.java
@@ -0,0 +1,10 @@
+package com.xworkz.rbi.internal;
+
+public abstract class Rbi {
+    public  abstract void gst();
+
+    public void lone()
+    {
+        System.out.println("this is parent class");
+    }
+}
