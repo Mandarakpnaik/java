af56fe2cee84d7d739127ec888b485c72eaac6b7
Mandarakpnaik <mandaranaik.xworkz@gmail.com>
2025-11-03 11:46:42 +0530

    Encapsulation

---
+++ Encapsulation/devices/src/com/xworkz/devices/external/BluetoothRunner.java
@@ -0,0 +1,29 @@
+package com.xworkz.devices.external;
+
+import com.xworkz.devices.dto.BluetoothDto;
+
+public class BluetoothRunner {
+    public static void main(String[] args) {
+        BluetoothDto blutooth=new BluetoothDto();
+        blutooth.setPrice(2000);
+        int ammount = blutooth.getPrice();
+        System.out.println("ammount=" + ammount);
+        blutooth.setName("fastrack");
+        String name = blutooth.getName();
+        System.out.println("name=" + name);
+        blutooth.setReview(9.5f);
+        float review = blutooth.getReview();
+        blutooth.setRateing(6.7);
+        double rate = blutooth.getGetRateing();
+        System.out.println("rateing=" + rate);
+        blutooth.setNumber(990088772);
+        long num = blutooth.getNumber();
+        System.out.println("num=" + num);
+        blutooth.getNumber();
+        blutooth.setQulity('A');
+        char grade = blutooth.getQulity();
+        blutooth.getQulity();
+        System.out.println("grade=" + grade);
+
+    }
+}
