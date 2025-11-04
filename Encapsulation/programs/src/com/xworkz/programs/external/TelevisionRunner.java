79a3b499121ec75fb17fb4af10f14f1c3b797b96
Mandarakpnaik <mandaranaik.xworkz@gmail.com>
2025-11-04 10:46:45 +0530

    Encapsulation

---
+++ Encapsulation/programs/src/com/xworkz/programs/external/TelevisionRunner.java
@@ -0,0 +1,28 @@
+package com.xworkz.programs.external;
+
+import com.xworkz.programs.dto.TelevisionDto;
+
+public class TelevisionRunner {
+    public static void main(String[] args) {
+        TelevisionDto tellivission =new TelevisionDto();
+        tellivission.setPrice(60000);
+        int ammount = tellivission.getPrice();
+        System.out.println("ammount=" + ammount);
+        tellivission.setName("sonay");
+        String name =tellivission.getName();
+        System.out.println("name=" + name);
+        tellivission.setReview(9.5f);
+        float review =tellivission.getReview();
+        tellivission.setRateing(6.7);
+        double rate = tellivission.getGetRateing();
+        System.out.println("rateing=" + rate);
+        tellivission.setNumber(990088772);
+        long num =tellivission.getNumber();
+        System.out.println("num=" + num);
+
+        tellivission.setQulity('A');
+        char grade = tellivission.getQulity();
+        tellivission.getQulity();
+        System.out.println("grade=" + grade);
+    }
+}
